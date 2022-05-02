6609571c6e7fe8fcb1b4d62fc621044c856cc8fa
Alan Buzek <alan.buzek@gmail.com>
2022-05-02 07:52:18 +0200

    overlay improvements

--- packages/extension/src/content/helper/overlay.ts
+++ packages/extension/src/content/helper/overlay.ts
@@ -1,17 +1,32 @@
 export const overlay = {
-  previousOverflowValue: "",
+  previousOverflowValue: "", // TODO: is this needed?
+  isInitiated: false,
+  isActive: false,
   init() {
+    if (this.isInitiated) {
+      return;
+    }
+
     const overlay = document.createElement("div");
     overlay.id = "termit-overlay";
     document.body.appendChild(overlay);
+    this.isInitiated = true;
   },
   on() {
     this.previousOverflowValue = document.body.style.overflow;
     document.body.style.overflow = "hidden";
-    document.getElementById("termit-overlay").style.display = "block";
+    document.getElementById("termit-overlay")!.style.display = "block";
+    this.isActive = true;
   },
   off() {
     document.body.style.overflow = this.previousOverflowValue;
-    document.getElementById("termit-overlay").style.display = "none";
+    document.getElementById("termit-overlay")!.style.display = "none";
+    this.isActive = false;
+  },
+  destroy() {
+    const element = document.getElementById("termit-overlay")!;
+
+    document.body.removeChild(element);
+    this.isInitiated = false;
   },
 };
